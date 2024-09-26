cdbd59bd2e9c7804b02306bb74aab0e8fdb1cea1
itsGovi <govardhanyadavsangi@gmail.com>
2024-09-26 08:18:50 +0530

    completed the graph code and understood dfs and bfs much better along with why to use 'append' not 'add' in them too!

--- Data Structures/graph.py
+++ Data Structures/graph.py
@@ -24,8 +24,50 @@ class Graph:
   def add_vertex(self, vertex):
     if vertex not in self.graph:
       self.graph[vertex] = [] # Initialize an empty list for edges
+    else:
+      print(f"{vertex} already exists in the graph!")
   
   def add_edge(self, vertex1, vertex2):
     """Add an undirected edge between vertex1 and vertex2"""
     if vertex1 in self.graph and vertex2 in self.graph:
+      self.graph[vertex1].append(vertex2)
+      self.graph[vertex2].append(vertex2)
+    else:
+      print("One or both vertices do not exist.")
       
+  def display(self):
+    """Display the graph"""
+    for vertex,edges in self.graph.items():
+      print(f"{vertex}: {', '.join(edges)}")
+      
+  def bfs(self, start_vertex):
+    """Breadth-First Search (BFS) traversal starting from start_vertex."""
+    visited = set()
+    queue = deque([start_vertex])
+    
+    while queue:
+      current_vertex = queue.popleft() # Pop the 1st element from the queue
+      if current_vertex not in visited:
+        print(current_vertex, end=' ') # Process(print) the vertex
+        visited.add(current_vertex) # Marking the vertex as visited
+        
+        # Adding all neighbors to the queue
+        for neighbor in self.graph[current_vertex]:
+          if neighbor not in queue:
+            queue.append(neighbor)
+            
+  def dfs(self, start_vertex):
+    """Depth-First Search (DFS) traversal starting from start_vertex."""
+    visited = set()
+    self._dfs_recursive(start_vertex, visited)
+    
+  def _dfs_recursive(self, vertex, visited):
+    """Helper Function"""
+    if vertex not in visited:
+      print(vertex, end=' ')
+      visited.append(vertex)
+      
+      # Recursively visit all the unvisited neighbors
+      for neighbor in self.graph[vertex]:
+        if neighbor not in visited:
+          self._dfs_recursive(neighbor, visited) 
