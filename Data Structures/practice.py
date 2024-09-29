5ec33db20c2f521e20a7b1b0ae29751d9248e0ee
itsGovi <govardhanyadavsangi@gmail.com>
2024-09-29 12:05:23 +0530

    finished catching-upto the revision of all ds

--- Data Structures/practice.py
+++ Data Structures/practice.py
@@ -83,6 +83,133 @@ class Stack:
         print(item)
         
         
-"""Queue"""
+"""Queue's are pretty much the same as Stack, with 5% change FIFO"""
 
-class
+
+"""BST"""
+
+class Node:
+  def __init__(self,key):
+    self.right = None
+    self.left = None
+    self.key = key
+    
+class BST:
+  def __init__(self):
+    self.root = None
+    
+  def insert(self, key):
+    return self.__insert(self.root, key)
+      
+  def _insert(self, current_node, key):
+    if current_node is None:
+      current_node = Node(key)
+
+    if key < current_node.value:
+      self._insert(current_node.left, key)
+    
+    elif key > current_node.value:
+      self._insert(current_node.right, key)
+      
+  def search(self, key):
+    return self._search(self.root, key)
+  
+  def _search(self, current_node, key):
+    if current_node is not key:
+      return False
+    
+    if current_node == key:
+      return True
+    
+    if key < current_node.value:
+      self._search(current_node.left, key)
+  
+    elif key > current_node.value:
+      self._search(current_node.right, key)
+  
+  def inorder_traverse(self,):
+    return self._inorder_traverse(self.root)
+  
+  def _inorder_traverse(self,current_node):
+    if current_node:
+      self._inorder_traverse(current_node.left)
+      
+      print(current_node.value, end=' ')
+      
+      self._inorder_traverse(current_node.right)
+      
+  def min_value(self):
+    return self._min_vaule(self.root)
+  
+  def _min_value(self,node):
+    current = node
+    while current.left is not None:
+      current = current.left
+    return current.value
+  
+  def max_value(self):
+    return self._max_value(self.root)
+  
+  def _max_value(self, node):
+    current = node
+    while current.right is not None:
+      current = current.right
+    return current.value
+  
+  
+"""Graph"""
+
+from collections import deque
+
+class Graph:
+  def __init__(self):
+    self.graph = {}
+    
+  def add_vertex(self, vertex):
+    if vertex not in self.graph:
+      self.graph[vertex] = [] #1
+    else:
+      print("This vertex already exists, can't add duplicates")
+      
+  def add_edges(self, vertex1, vertex2):
+    if vertex1 and vertex2 in self.graph:
+      if vertex1 == vertex2:
+        print("Can't add a self loop")
+      
+    if vertex2 not in self.graph[vertex1]:
+      self.graph[vertex1].append(vertex2)
+    if vertex1 not in self.graph[vertex2]:
+      self.graph[vertex2].append(vertex2)
+    else:
+      print("Can't finde given two vertices!")
+      
+  def display(self): #2
+    for vertex,edges in self.graph.items(): #3
+      print(f"{vertex}: {', '.join(edges)}") #4
+    
+  def bfs(self,start_vertex):
+    vistied = set()
+    queue = deque([start_vertex])
+    
+    while queue: #5
+      current = queue.popleft()
+      if current not in vistied:
+        print(current, end=' ')
+        vistied.add(current)
+        
+      for neighbor in self.graph[start_vertex]:
+        if neighbor not in vistied:
+          queue.append(neighbor)
+          
+  def dfs(self, start_vertex):
+    visited =set()
+    return self._dfs(start_vertex, visited)
+  
+  def _dfs(self, current, visited):
+    if current not in visited:
+      print(current, end=' ')      
+      visited.add(current)
+      
+    for neighbor in self.graph[current]:
+      if neighbor not in visited:
+        self._dfs(neighbor, visited) #6
